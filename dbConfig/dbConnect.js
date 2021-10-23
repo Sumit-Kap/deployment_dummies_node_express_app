cfa086cd8cd51f37d35b3e8bb511cf779db26063
Sumit Kapoor <sumwin@Sumits-MacBook-Air.local>
2021-10-23 16:20:21 +0530

    initial commit

--- dbConfig/dbConnect.js
+++ dbConfig/dbConnect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dbConnect = {
   connect: () => {
     mongoose
-      .connect("mongodb://127.0.0.1:27017/test")
+      .connect(process.env.DATABASE_URL)
       .then(() => {
         console.log("database connected");
       })
